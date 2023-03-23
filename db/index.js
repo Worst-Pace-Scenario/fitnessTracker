b8c8ab39af14b26bc98e7d82614c0fc214022582
cforlini24 <cforlini24@gmail.com>
2023-03-23 16:28:20 -0400

    added activties function for db

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const client = new Client(`postgress://localhost:5432/worstPaceScenario`)
 client.password = "1025464"
 
 
-//USERS
+                                                            //USERS
 async function createUser({username, password}) {
     try {
         const {rows} = await client.query(`
@@ -74,6 +74,65 @@ async function getUserByUsername(username) {
 }
 
 
+
+
+                                                                    //activites
+async function createActivity({name, description}) {
+    try {
+        const {rows} = await client.query(`
+        INSERT INTO activities(name, description)
+        VALUES ($1, $2)
+        ON CONFLICT (name) DO NOTHING
+        RETURNING *;
+        `,[name, description]);
+
+        return rows;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getAllActivities() {
+    try {
+        const {rows} = await client.query(`
+        SELECT * FROM activities;
+        `)
+
+        return rows;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getActivityById(id){
+    try {
+        const {rows: [activity] } =  await client.query(`
+        SELECT * FROM activities
+        WHERE id = $1;
+        `,[id])
+
+        return activity;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function updateActivity({id, name, description}){
+    try {
+        const {rows} = await client.query(`
+        UPDATE activities
+        SET "name" = $1, "description" = $2
+        WHERE id = $3
+        RETURNING *;
+        `,[name, description, id])
+        
+        return rows;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
 async function buildDb() {
     try {
         const usersToCreate = [
@@ -86,13 +145,48 @@ async function buildDb() {
         // const users = await Promise.all(usersToCreate.map(createUser))
 
 
-        const albert =  await getUserByUsername("albert")
-        const sandra = await getUserById(2)
-        const glamgal = await getUser({username: "glamgal", password: "glamgal123"})
+        // const albert =  await getUserByUsername("albert")
+        // const sandra = await getUserById(2)
+        // const glamgal = await getUser({username: "glamgal", password: "glamgal123"})
 
-        console.log("albert", albert)
-        console.log("sandra", sandra)
-        console.log("glamgal", glamgal)
+        // console.log("albert", albert)
+        // console.log("sandra", sandra)
+        // console.log("glamgal", glamgal)
+
+        // const activitiesToCreate = [
+        //     {
+        //       name: "wide-grip standing barbell curl",
+        //       description: "Lift that barbell!",
+        //     },
+        //     {
+        //       name: "Incline Dumbbell Hammer Curl",
+        //       description:
+        //         "Lie down face up on an incline bench and lift thee barbells slowly upward toward chest",
+        //     },
+        //     {
+        //       name: "bench press",
+        //       description: "Lift a safe amount, but push yourself!",
+        //     },
+        //     { name: "Push Ups", description: "Pretty sure you know what to do!" },
+        //     { name: "squats", description: "Heavy lifting." },
+        //     { name: "treadmill", description: "running" },
+        //     { name: "stairs", description: "climb those stairs" },
+        //   ]
+        //   const activities = await Promise.all(activitiesToCreate.map(createActivity))
+      
+        //   console.log("activities created:")
+        //   console.log(activities)
+
+        const activites = await getAllActivities();
+        console.log("All activities: ", activites);
+
+        const benchPress = await getActivityById(3);
+        console.log("bench press", benchPress);
+
+        const updatedAct = await updateActivity({id : 1, name: "Updates!", description: "I was updated"})
+        console.log(updatedAct)
+
+
         client.end()
         // console.log("Users created:")
         // console.log(users)
